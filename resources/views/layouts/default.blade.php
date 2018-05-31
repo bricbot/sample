0528ed8d6febe45eb9eb554f0cb950fce81c8078
Myertai-vagr <taimyer@gmail.com>
2018-05-31 13:41:55 +0000

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -8,10 +8,10 @@
     <header class="navbar navbar-fixed-top navbar-inverse">
       <div class="container">
         <div class="col-md-offset-1 col-md-10">
-          <a href="/" id="logo">Sample App</a>
+          <a href=" {{ route('home') }} " id="logo">Sample App</a>
           <nav>
             <ul class="nav navbar-nav navbar-right">
-              <li><a href="/help">帮助</a></li>
+              <li><a href="{{ route('help') }}">帮助</a></li>
               <li><a href="#">登录</a></li>
             </ul>
           </nav>
@@ -22,5 +22,21 @@
     <div class="container">
       @yield('content')
     </div> 
+    <div class="col-md-12">
+    
+    <footer class="footer">
+        <small class="slogon">
+          <img class="brand-icon" src="https://lccdn.phphub.org/uploads/images/201612/12/1/iq7WQc2iuW.png?imageView2/1/w/34/h/34">
+          <a href="https://laravel-china.org/courses">
+            刻意练习，每日精进
+          </a>
+        </small>
+        <nav>
+            <ul>
+                <li><a href="{{ route('about') }}">关于</a></li>
+            </ul>
+        </nav>
+    </footer>
+    </div>
 </body>
 </html>
---
+++ resources/views/users/create.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.default')
+
+@section('subtitle', 'Sign Up');
+@section('content')
+    <h1>注册</h1>
+@stop
